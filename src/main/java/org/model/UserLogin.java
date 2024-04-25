17ee502bc0845be8993c91b25c80f86dc2233ec8
nicolas <nicolas.souza@moinformatica.com>
2024-04-24 21:13:49 -0300

    Implemented session validation

---
+++ src/main/java/org/model/UserLogin.java
@@ -0,0 +1,23 @@
+package org.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "user_login")
+public class UserLogin {
+
+    @Id
+    private Long id;
+
+    @Column(name = "password", columnDefinition = "TEXT")
+    private String password;
+
+    @Column(name = "username", columnDefinition = "TEXT")
+    private String username;
+
+}
